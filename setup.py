44ed16314cc0074d146685d3802014abb2b40899
Andy Lee <andrew.lee3@sportcast.co.uk>
2023-03-01 17:22:18 +0800

    swapped version and added __init__.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-import setuptools
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Capsphere proprietary libraries'
+PACKAGE_NAME = 'capsphere'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="capsphere",
-    version="0.0.1",
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
     author="Andy Lee",
     author_email="andy.mt.lee@gmail.com",
-    description="Capsphere proprietary libraries",
+    description=DESCRIPTION,
+    keywords=['python', 'capsphere', 'pdf'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ourandy/packaging",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
